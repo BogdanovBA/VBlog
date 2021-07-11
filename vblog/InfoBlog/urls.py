9aac732dadd329e8722ab6906e8e02813c6e6446
BogdanovBA <70698943+BogdanovBA@users.noreply.github.com>
2021-07-11 23:00:58 +0300

    Used CBV and other small changes

--- vblog/InfoBlog/urls.py
+++ vblog/InfoBlog/urls.py
@@ -4,8 +4,8 @@ from InfoBlog.views import *
 urlpatterns = [
     path('', index, name='home'),
     path('home/', index, name='home'),
-    path('posts/', all_posts, name='posts'),
-    path('posts/<slug:post_slug>', show_post, name='post'),
-    path('add_post/', add_post, name='add_post'),
-    path('category/<slug:cat_slug>', show_category, name='category'),
+    path('posts/', InfoBlogPosts.as_view(), name='posts'),
+    path('post/<slug:post_slug>', ShowPost.as_view(), name='post'),
+    path('add_post/', AddPost.as_view(), name='add_post'),
+    path('category/<slug:cat_slug>', PostsCategory.as_view(), name='category'),
 ]
--- vblog/InfoBlog/views.py
+++ vblog/InfoBlog/views.py
@@ -1,58 +1,97 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseNotAllowed, Http404
+from django.views.generic import ListView, DeleteView, CreateView
 
 from InfoBlog.forms import *
 from InfoBlog.models import Posts, Category
+
+
+class InfoBlogPosts(ListView):
+    model = Posts
+    template_name = 'InfoBlog/posts_page.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category_selected'] = 0
+        return context
+
+    def get_queryset(self):
+        return Posts.objects.filter(is_published=True)
 
 
 def index(request):
     return render(request, 'InfoBlog/index.html')
 
 
-def all_posts(request):
-    posts = Posts.objects.all()
-
-    context = {
-        'posts': posts,
-        'category_selected': 0,
-    }
-    return render(request, 'InfoBlog/posts_page.html', context=context)
-
-
-def show_category(request, cat_slug):
-    categories = Category.objects.all()
-    cat = get_object_or_404(Category, slug = cat_slug)
-    posts = Posts.objects.filter(category_id=cat.id)
-
-    context = {
-        'posts': posts,
-        'categories': categories,
-        'category_selected': cat_slug,
-    }
-    return render(request, 'InfoBlog/posts_page.html', context=context)
+#def all_posts(request):
+#    posts = Posts.objects.all()
+#
+#    context = {
+#        'posts': posts,
+#        'category_selected': 0,
+#    }
+#    return render(request, 'InfoBlog/posts_page.html', context=context)
 
 
+class PostsCategory(ListView):
+    model = Posts
+    template_name = 'InfoBlog/posts_page.html'
+    context_object_name = 'posts'
+    allow_empty = False
 
-def show_post(request, post_slug):
-    post = get_object_or_404(Posts, slug=post_slug)
+    def get_queryset(self):
+        return Posts.objects.filter(category__slug=self.kwargs['cat_slug'], is_published=True)
 
-    context = {
-        'post': post,
-        'category_selected': post.category_id,
-    }
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category_selected'] = context['posts'][0].category_id
+        return context
 
-    return render(request, 'InfoBlog/post.html', context=context)
+# def show_category(request, cat_slug):
+#     categories = Category.objects.all()
+#     cat = get_object_or_404(Category, slug = cat_slug)
+#     posts = Posts.objects.filter(category_id=cat.id)
+#
+#     context = {
+#         'posts': posts,
+#         'categories': categories,
+#         'category_selected': cat_slug,
+#     }
+#     return render(request, 'InfoBlog/posts_page.html', context=context)
 
 
-def add_post(request):
-    if request.method == 'POST':
-        form = AddPostForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('posts')
-    else:
-        form = AddPostForm()
-    return render(request, 'InfoBlog/add_post.html', {'form': form})
+class ShowPost(DeleteView):
+    model = Posts
+    template_name = 'InfoBlog/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
+
+# def show_post(request, post_slug):
+#     post = get_object_or_404(Posts, slug=post_slug)
+#
+#     context = {
+#         'post': post,
+#         'category_selected': post.category_id,
+#     }
+#
+#     return render(request, 'InfoBlog/post.html', context=context)
+
+
+class AddPost(CreateView):
+    form_class = AddPostForm
+    template_name = 'InfoBlog/add_post.html'
+
+
+# def add_post(request):
+#     if request.method == 'POST':
+#         form = AddPostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('posts')
+#     else:
+#         form = AddPostForm()
+#     return render(request, 'InfoBlog/add_post.html', {'form': form})
 
 
 def page_not_found(request, exception):
