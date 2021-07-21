2250d816f230a97bcaa6d469585afc56fce51413
BogdanovBA <70698943+BogdanovBA@users.noreply.github.com>
2021-07-21 19:32:44 +0300

    Added styles for admin-panel

--- vblog/InfoBlog/admin.py
+++ vblog/InfoBlog/admin.py
@@ -1,14 +1,24 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import *
 
 
 class PostsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'time_update', 'post_image', 'is_published', 'category')
+    list_display = ('id', 'title', 'time_create', 'time_update', 'get_html_photo', 'is_published', 'category')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'time_create')
     prepopulated_fields = {"slug": ("title",)}
+    fields = ('title', 'slug', 'category', 'content', 'post_image', 'is_published', 'time_create', 'time_update', 'get_html_photo')
+    readonly_fields = ('time_create', 'time_update', 'get_html_photo')
+
+    def get_html_photo(self, object):
+        if object.post_image:
+            return mark_safe(f"<img src='{object.post_image.url}' width=75>")
+
+    get_html_photo.short_description = 'Image'
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -20,3 +30,6 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Posts, PostsAdmin)
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.site_title = 'ADMIN'
+admin.site.site_header = 'admin-panel VBlog'
